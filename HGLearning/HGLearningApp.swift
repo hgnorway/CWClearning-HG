29c67377c9a1495e90439fad5588104faeeb0070
hg <gongkeq@gmail.com>
2021-06-26 16:44:06 +0200

    Initial Commit

---
+++ HGLearning/HGLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  HGLearningApp.swift
+//  HGLearning
+//
+//  Created by Hong Gong on 26/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct HGLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
